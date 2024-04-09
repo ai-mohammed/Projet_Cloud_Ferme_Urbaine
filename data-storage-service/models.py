7725c852bd98b15305b59688950c16cf58767b65
Gianluca-Schileo <github.b1ewl@simplelogin.com>
2024-04-09 19:53:52 +0200

    lib

--- data-storage-service/models.py
+++ data-storage-service/models.py
@@ -1,13 +1,13 @@
 import os
-import psycopg2
+import psycopg3
 
 # Connect to PostgreSQL server
-conn = psycopg2.connect(
+conn = psycopg3.connect(
     host=os.getenv("DB_HOST"),
     port=os.getenv("DB_PORT"),
     database=os.getenv("DB_NAME"),
     user=os.getenv("DB_USER"),
-    password=os.getenv("DB_PASS")
+    password=os.getenv("DB_PASS"),
 )
 
 # Create cursor
