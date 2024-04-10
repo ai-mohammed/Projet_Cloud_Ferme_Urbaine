6e1a0c00d40a54611e3dc1aa5c559b15eb0f93d8
Mohammed ADDI <132593906+ai-mohammed@users.noreply.github.com>
2024-04-10 18:40:07 +0200

    getting data

--- data-collection-service/data-collection-service.py
+++ data-collection-service/data-collection-service.py
@@ -17,6 +17,32 @@ def receive_data():
 
         # Log or process your data here
         print(unpacked_data)
+        
+        # Extraction de 'measures'
+        measures = unpacked_data.get('measures', {})
+
+        # Température
+        temperature_celsius = None
+        temp_keys = ['temperature', 'température']
+        for key in temp_keys:
+            if key in measures:
+                temp_value = measures[key]
+                if '°C' in temp_value:
+                    temperature_celsius = float(temp_value.replace('°C', ''))
+                elif '°F' in temp_value:
+                    temperature_celsius = (float(temp_value.replace('°F', '')) - 32) * 5.0 / 9.0
+                elif '°K' in temp_value:
+                    temperature_celsius = float(temp_value.replace('°K', '')) - 273.15
+                break
+
+        # Humidité
+        humidity_percent = None
+        humid_keys = ['humidity', 'humidite']
+        for key in humid_keys:
+            if key in measures:
+                humidity_value = measures[key]
+                humidity_percent = float(humidity_value.replace('%', ''))
+                break
 
         # Return a success response
         return jsonify({"status": "success", "message": "Data received successfully", "data": unpacked_data}), 201
